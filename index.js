1ea68ed625bc9a1afb8e84790ba00c1b97e5ac82
trongtinvd <trongtinvd@gmail.com>
2025-10-31 20:37:49 +0700

    make the banner interactive

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("js")
+console.log("start");
+
+
+const mainBanner = document.querySelector(".main-banner");
+
+function changeBanner(newIndex) {
+  const oldBanner = document.querySelector(".active-main-banner");
+  const oldIndex = oldBanner.dataset.bannerIndex;
+  const oldButton = document.querySelector(`[data-banner-navigation-index='${oldIndex}']`);
+  const newBanner = document.querySelector(`[data-banner-index='${newIndex}']`);
+  const newButton = document.querySelector(`[data-banner-navigation-index='${newIndex}']`);
+  if (newBanner) {
+    mainBanner.scrollLeft = oldBanner.clientWidth * newIndex;
+
+    oldBanner.classList.remove("active-main-banner");
+    newBanner.classList.add("active-main-banner");
+
+    oldButton.classList.remove("active-banner-navigation-button");
+    newButton.classList.add("active-banner-navigation-button");
+  }
+}
+
+document.querySelector(".next-banner-button").addEventListener("click", event => {
+  const currentBanner = document.querySelector(".active-main-banner");
+  const currentIndex = currentBanner.dataset.bannerIndex;
+  changeBanner(Number(currentIndex) + 1);
+});
+
+document.querySelector(".previous-banner-button").addEventListener("click", event => {
+  const currentBanner = document.querySelector(".active-main-banner");
+  const currentIndex = currentBanner.dataset.bannerIndex;
+  changeBanner(Number(currentIndex) - 1);
+});
+
+document.querySelectorAll(".banner-navigation-button").forEach(button => {
+  button.addEventListener("click", event => {
+    const nextIndex = button.dataset.bannerNavigationIndex;
+    changeBanner(nextIndex);
+  })
+});
